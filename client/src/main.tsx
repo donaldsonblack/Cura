ca665fcd98dea5f9f1a935eb3aa359b4ecbf4817
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-08 19:11:41 +1000

    changed website logo

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+
+createRoot(document.getElementById('root')!).render( // '!' asserts that document.getElementById('root') will 100% return a HTMLElement, and not null.
   <StrictMode>
     <App />
   </StrictMode>,
