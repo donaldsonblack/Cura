19419be5964bac5670d7a72c8957996ccdc63d1e
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-09 13:44:05 +1000

    changed router from hashrouter to browserrouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,22 @@ import './index.css'
 import Home from './pages/home'
 import Info from './pages/info'
 import Login from './pages/Login'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
 // ^ curly braces means item(s) were named in the file they're being retrieved from
 
 
 function App() {
 
   return (
+    
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/info" element={<Info/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
-    </Router> 
+    </Router>
+   
   )
 }
 
