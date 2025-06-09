0f29a6dcfba8f52960671955422d0783f38a24d7
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-09 13:57:47 +1000

    added sidebar shadcn/ui component,

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components/ui/button'
 import '../index.css'
+import { Link } from 'react-router-dom' // using react-router-dom Link rather than <a href=""></a> prevents full page reload
+
+
 
 function Home() {
     return (
         <>
             <h1>Home</h1>
+            <div className="flex flex-wrap items-center gap-2 md:flex-row">
+                <Button>
+                    <Link to="/login">Login</Link>
+                </Button>
+            </div>
         </>
     )
 }
