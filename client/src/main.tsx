30cf987efbc0f416670755451fe042d08f1d1dea
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-09 13:14:27 +1000

    added pages folder with home.tsx and info.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// entry points for react, this is where react renders the app
 
 createRoot(document.getElementById('root')!).render( // '!' asserts that document.getElementById('root') will 100% return a HTMLElement, and not null.
   <StrictMode>
