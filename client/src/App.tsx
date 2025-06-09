8aef00ad126589c6879540122d133a318a4b876b
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-09 13:28:52 +1000

    added router / routes / route statements for all pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import './index.css'
-
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Info from './pages/info'
+import Login from './pages/Login'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom' 
+// ^ curly braces means item(s) were named in the file they're being retrieved from
 
 
 function App() {
 
   return (
-    <>
-    </> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/info" element={<Info/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router> 
   )
 }
 
