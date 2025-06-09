dcab47bdc9de29d71c06c8c89bc445c4e6390232
Harrison Black <harrison.black18@Gmail.com>
2025-06-09 18:43:10 +1000

    AWS RDS Connectivity POC

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.springframework.boot") version "3.2.6"
     id("io.spring.dependency-management") version "1.1.4"
     id("java")
+    id("application")
 }
 
 group = "dev.donaldsonblack"
@@ -15,11 +16,29 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.postgresql:postgresql:42.7.3") 
+    implementation(platform("software.amazon.awssdk:bom:2.27.21"))
+    implementation("software.amazon.awssdk:s3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     
     // Use for JWT and cognito authentication lateer
     //implementation("org.springframework.boot:spring-boot-starter-security")
     //implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     //implementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
+}
+
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "dev.donaldsonblack.medtrace.backend.MedtraceBackend"
+    }
+
+    // Optionally include compileClasspath in the JAR
+    // This is not recommended for Spigot, usually just use 'compileOnly'
+    // To shade dependencies instead, use the Shadow plugin
+}
+
+application {
+    mainClass.set("dev.donaldsonblack.medtrace.backend.MedtraceBackend")
 }
--- server/src/main/java/dev/donaldsonblack/medtrace/backend/MedtraceBackend.java
+++ server/src/main/java/dev/donaldsonblack/medtrace/backend/MedtraceBackend.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedtraceBackend {
-    public static void main(String[] args) {
-        SpringApplication.run(MedtraceBackend.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MedtraceBackend.class, args);
+  }
 }
