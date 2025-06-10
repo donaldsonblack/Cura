d9b49410e6bd1c417d8e3e6f63b30efef128b1b9
Harrison Black <harrison.black18@Gmail.com>
2025-06-10 12:39:14 +1000

    Dynamically gets AWS secrets for db connection

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -14,13 +14,28 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot dependencies
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.postgresql:postgresql:42.7.3") 
     implementation(platform("software.amazon.awssdk:bom:2.27.21"))
-    implementation("software.amazon.awssdk:s3")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // AWS SDK for Java v2
+    implementation(platform("software.amazon.awssdk:bom:2.25.8"))
+
+    // AWS SDK for Secrets Manager
+    implementation("software.amazon.awssdk:secretsmanager")
+    implementation("software.amazon.awssdk:auth")
+    implementation("software.amazon.awssdk:regions")
+    implementation("software.amazon.awssdk:aws-core")
+
+    implementation("org.json:json:20090211")
+
+    // HikariCP datasource
+    implementation("com.zaxxer:HikariCP")
     
     // Use for JWT and cognito authentication lateer
     //implementation("org.springframework.boot:spring-boot-starter-security")
