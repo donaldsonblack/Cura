048b45641445f557045c7387d5d869bf859a0dda
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-06-10 16:43:03 +1000

    added grid layout and components

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -8,8 +8,8 @@ function Home() {
         <>
         
 
-        <div className='min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50'>
-            <div className="mx-auto max-w-4xl grid grid-cols-12 gap-4">
+        <div className=' min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50'>
+            <div className=" mx-auto max-w-4xl grid grid-cols-12 gap-4">
                 <HeaderBlock></HeaderBlock>
                 <Block></Block>
                 <Block></Block>
