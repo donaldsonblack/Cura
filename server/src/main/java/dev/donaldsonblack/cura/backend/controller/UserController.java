6160a9430782c4090ec16c3b7d58b7fd6ac01403
Harrison Black <harrison.black18@Gmail.com>
2025-06-16 22:57:09 +1000

    TODO, implement user name/email echo

--- server/src/main/java/dev/donaldsonblack/cura/backend/controller/UserController.java
+++ server/src/main/java/dev/donaldsonblack/cura/backend/controller/UserController.java
@@ -1,13 +1,16 @@
 package dev.donaldsonblack.cura.backend.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/me")
-    public String getUser() {
-        return "User information";
-    }
+  @GetMapping("/me")
+  public String getUser(@AuthenticationPrincipal Jwt jwt) {
+    // @TODO - Implement logic to retrieve user information from the JWT token
+    return "todo";
+  }
 }
