556dc8ef82a535cfee6a5725530d18e2b938f8a4
Harrison Black <harrison.black18@Gmail.com>
2025-06-20 16:42:16 +1000

    Moving files into cura/

---
+++ server/src/main/java/dev/donaldsonblack/cura/model/ChecklistTemplate.java
@@ -0,0 +1,52 @@
+package dev.donaldsonblack.cura.model;
+
+import java.sql.Timestamp;
+import org.json.JSONObject;
+
+public class ChecklistTemplate {
+    private int equip_id;
+    private String description;
+    private JSONObject template_json;
+    private String template_json_String;
+    private Timestamp created_at;
+
+    public int getEquipId() {
+        return equip_id;
+    }
+
+    public void setEquipId(int equip_id) {
+        this.equip_id = equip_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public JSONObject getTemplateJson() {
+        return template_json;
+    }
+
+    public void setTemplateJson(JSONObject template_json) {
+        this.template_json = template_json;
+    }
+
+    public Timestamp getCreatedAt() {
+        return created_at;
+    }
+
+    public void setCreatedAt(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setTemplateJsonString(String template_json_String) {
+        this.template_json_String = template_json_String;
+    }
+
+    public String getTemplateJsonString() {
+        return template_json_String;
+    }
+}
---
+++ server/src/main/java/dev/donaldsonblack/cura/service/ChecklistTemplateService.java
@@ -0,0 +1,45 @@
+package dev.donaldsonblack.cura.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.donaldsonblack.cura.model.ChecklistTemplate;
+
+@Service
+public class ChecklistTemplateService {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public List<ChecklistTemplate> getAllRecords() {
+        List<ChecklistTemplate> records = new ArrayList<>();
+        String sql = "SELECT * FROM checklist_templates";
+
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                ChecklistTemplate record = new ChecklistTemplate();
+                record.setEquipId(rs.getInt("equip_id"));
+                record.setDescription(rs.getString("description"));
+                record.setTemplateJsonString(rs.getString("template_json"));
+                record.setCreatedAt(rs.getTimestamp("created_at"));
+
+                records.add(record);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return records;
+    }
+}
